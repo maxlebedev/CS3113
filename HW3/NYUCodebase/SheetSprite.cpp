6c061b5765428b710a41994b9a5668d3eae88004
Max Lebedev <maxlebedev@live.com>
2015-10-11 15:22:58 -0400

    adding NYUCodebase.xcodeproj files

---
+++ HW3/NYUCodebase/SheetSprite.cpp
@@ -0,0 +1,9 @@
+//
+//  SheetSprite.cpp
+//  NYUCodebase
+//
+//  Created by Max Lebedev on 10/10/15.
+//  Copyright © 2015 Ivan Safrin. All rights reserved.
+//
+
+#include "SheetSprite.hpp"
---
+++ HW3/NYUCodebase/SheetSprite.hpp
@@ -0,0 +1,14 @@
+//
+//  SheetSprite.hpp
+//  NYUCodebase
+//
+//  Created by Max Lebedev on 10/10/15.
+//  Copyright © 2015 Ivan Safrin. All rights reserved.
+//
+
+#ifndef SheetSprite_hpp
+#define SheetSprite_hpp
+
+#include <stdio.h>
+
+#endif /* SheetSprite_hpp */
